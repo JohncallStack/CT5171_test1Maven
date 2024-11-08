017a83b489079350a039a81b52093383ac8b397e
JohncallStack <157517331+JohncallStack@users.noreply.github.com>
2024-11-08 13:13:59 +0000

    Updated to check if hook is working.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Testing to see if my hook is working, that would be class");
+
     }
 }
 
